b0dd927257fb0bcbf2ab055ccd655052a6aad5df
TavinScapi <tavinhoscapi@gmail.com>
2024-11-14 22:37:54 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark-theme");
+}
+
+function openMetronome() {
+    window.location.href = "metronome.html"; // Página de metrônomo
+}
+
+function openTuner() {
+    window.location.href = "tuner.html"; // Página de afinador
+}
